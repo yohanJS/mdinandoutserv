c269818e70fb95f1dc9ae30631bb4b56cf669a19
Yohan Garcia <yoanvaldes03@gmail.com>
2024-06-28 11:37:10 -0400

    finish about us section

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="about" class="container section-bg">
     <div class="min-vh-100 row d-flex align-items-center text-center">
-      <div class="col-12">
+      <div class="col-12 text-color">
         <p class="text-uppercase pb-1 m-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 100 3" width="20" height="5">
-            <rect width="100" height="3" fill="#000000" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 10 100 3" width="20" height="5">
+            <rect width="100" height="6" fill="#cfdd51" />
           </svg>
-          <strong class="text-color"> ABOUT US</strong>
+          <strong class="text-color"> ABOUT US </strong>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 10 100 3" width="20" height="5">
+            <rect width="100" height="6" fill="#cfdd51" />
+          </svg>
         </p>
         <p class="p-3">
         MD In & Out Services is a versatile company that provides a wide variety of services tailored to meet the unique
@@ -29,4 +32,7 @@
 .section-bg {
   background-color: #f5f5f5;
 }
+.text-color {
+    color: #0A323D;
+  }
 </style>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,7 +1,7 @@
   <template>
     <div id="services" class="container split-bg pt-5 pb-5" v-if="isMobile">
       <div class="col-12 text-center">
-        <p class="text-uppercase pb-3 line">
+        <p class="text-uppercase pb-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 1 100 3"
