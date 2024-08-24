15ce14a8e8fcb0db4760815bc53729116d9af716
Yohan Garcia <yoanvaldes03@gmail.com>
2024-08-24 15:54:53 -0400

    adding about content

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -12,17 +12,62 @@
           </svg>
         </p>
         <p class="p-3">
-        MD In & Out Services is a versatile company that provides a wide variety of services tailored to meet the unique
-        needs of your home. Our mission is to help our clients bring their dream spaces to life,
-        allowing them to transform their homes into something truly extraordinary.
-      </p>
-      <p class="p-3">
+          MD In & Out Services is a versatile company that provides a wide variety of services tailored to meet the
+          unique
+          needs of your home. Our mission is to help our clients bring their dream spaces to life,
+          allowing them to transform their homes into something truly extraordinary.
+        </p>
+        <!-- <p class="p-3">
         What are you looking for? Is it the same thing we once needed?
       </p>
       <p class="p-3">
         Do not wait any longer. 
         Come in and learn about all of our services; we're here to help!
-      </p>
+      </p> -->
+      <div class="col-12 p-3 text-color">
+        <div id="readMoreAccordion">
+          <div>
+            <h2 class="accordion-header" id="headingOne">
+              <a class="accordion-button mb-3 text-decoration-none text-color" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                aria-expanded="true" aria-controls="collapseOne">
+                Read more 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle ms-1" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                </svg>
+              </a>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+              data-bs-parent="#readMoreAccordion">
+              <div class="accordion-body">
+                <p> One of our family hobbies is spending time in our yard, creating beautiful memories within the
+                  comfort of our home.
+                  This is why we are committed to assisting every customer in personalizing their spaces, both indoors
+                  and outdoors,
+                  to suit their budget. Our goal is to provide a suitable and inviting atmosphere where families can
+                  enjoy quality time together.
+                </p>
+                <p>
+                  When my wife and I were searching for our dream home, one of our priorities was having a patio with a
+                  pergola where our children could play.
+                  Although  we found the ideal home, there was one thing missing in the backyard that would make family
+                  time more enjoyable: a roofed space. 
+                  It wasn't until we built a pergola that we starting enjoying our patio and home even more.
+                </p>
+                <p>
+                  Family gatherings no longer require a purposeful occasion. For us, every day is a celebration. All we
+                  need is some nice BBQ and good company.
+                  This is what we love about MD In & Out Services; our experience can be yours too.
+                </p>
+                <p>What are you looking for? Is it the same thing we once needed? Do not wait any longer.
+                </p>
+                <p>
+                  Come in and learn about all of our services; we're here to help!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   </div>
@@ -32,7 +77,8 @@
 .section-bg {
   background-color: #f5f5f5;
 }
+
 .text-color {
-    color: #0A323D;
-  }
+  color: #0A323D;
+}
 </style>
