dad097cd26af346c11ba1d407c0a08327e474bff
Yohan Garcia <yoanvaldes03@gmail.com>
2024-09-09 13:02:50 -0400

    Added links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,21 +22,21 @@
           </defs>
         </svg>
         <p class="txt-color mt-3">Make your dream home come true</p>
-        <a href="#">
+        <a href="https://www.tiktok.com/@md.in.out.service?_t=8pZvyOjJKH8&_r=1">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-tiktok m-2"
             viewBox="0 0 16 16">
             <path
               d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
           </svg>
         </a>
-        <a href="#">
+        <a href="https://www.facebook.com/profile.php?id=100090709224068&mibextid=ZbWKwL">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-facebook m-2"
             viewBox="0 0 16 16">
             <path
               d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
           </svg>
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com/md_inandoutservices?igsh=eDRjajc1dzJzejN4">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-instagram m-2"
             viewBox="0 0 16 16">
             <path
@@ -64,7 +64,7 @@
       </div>
       <hr class="line-color" />
       <div class="col-12">
-        <p class="txt-color">Copyright © {{ currentYear }} | MD In & Out Services. All rights reserved. “xxxxxxxxxxxxxx”
+        <p class="txt-color">Copyright © {{ currentYear }} | MD In & Out Services. All rights reserved. <span>“I can do all this through him who gives me strength.”  Philippians 4:13 </span>
         </p>
       </div>
     </div>
