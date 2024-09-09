6b0caca0a63e7c1a93f3349c2f3810593974569a
Yohan Garcia <yoanvaldes03@gmail.com>
2024-09-09 10:12:48 -0400

    updates based on latest

--- src/components/LetsTalk.vue
+++ src/components/LetsTalk.vue
@@ -1,11 +1,28 @@
 <template>
-  <div id="letsTalk" class="row justify-content-center bg-color pb-2 pt-2 m-0">
-    <div class="col-12 p-0 text-center" v-if="isMobile">
-      <p class="m-0 txt-color">Let's talk <a href="tel:+1xxxxxxxxxx">(xxx) xxx-xxxx</a></p>
-      <p class="m-0"><a href="mailto:mdinandoutservices@gmail.com">mdinandoutservices@gmail.com</a></p>
-    </div>
-    <div class="col-12 p-0 text-center" v-else>
-      <p class="m-0">Let's talk <a href="tel:+1xxxxxxxxxx">(xxx) xxx-xxxx</a><span id="bar" class="p-2">|</span><a href="mailto:mdinandoutservices@gmail.com">mdinandoutservices@gmail.com</a></p>
+  <div id="letsTalk" class="row text-center justify-content-end bg-color pb-2 pt-2 m-0">
+    <div class="col-12 col-md-2 p-0">
+      <p class="m-0">
+        <a href="tel:+17868125394">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-telephone-forward-fill" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zm10.761.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708" />
+            </svg>
+          </span>
+          Let's talk | 
+        </a>
+        <a href="mailto:mdinandoutservices@gmail.com">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-envelope-fill" viewBox="0 0 16 16">
+              <path
+                d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+            </svg>
+          </span>
+          Email us now!
+        </a>
+      </p>
     </div>
   </div>
 </template>
@@ -29,14 +46,21 @@ a {
   text-decoration: none;
   color: #1C434D;
 }
+
 .bg-color {
   background-color: #F3F3F3;
 }
+
 .txt-color {
   color: #1C434D;
 }
+
 #bar {
   display: inline-block;
+}
+
+.w-5 {
+  width: 5%;
 }
 
 @media only screen and (max-width: 768px) {
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -53,22 +53,22 @@
             <div class="col-12">
               <p class="lead btn-text-color fw-bold">Get Social! Connect with Us Today!</p>
             </div>
-            <div class="col-5 d-flex ">
-              <a href="#" class="nav-link m-2">
+            <div class="col-5 d-flex p-0">
+              <a href="https://www.tiktok.com/@md.in.out.service?_t=8pZvyOjJKH8&_r=1" class="nav-link m-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-tiktok"
                   viewBox="0 0 16 16">
                   <path
                     d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
                 </svg>
               </a>
-              <a href="#" class="nav-link m-2">
+              <a href="https://www.facebook.com/profile.php?id=100090709224068&mibextid=ZbWKwL" class="nav-link m-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
                   class="bi bi-facebook" viewBox="0 0 16 16">
                   <path
                     d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                 </svg>
               </a>
-              <a href="#" class="nav-link m-2">
+              <a href="https://www.instagram.com/md_inandoutservices?igsh=eDRjajc1dzJzejN4" class="nav-link m-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
                   class="bi bi-instagram" viewBox="0 0 16 16">
                   <path
@@ -140,15 +140,15 @@
   <nav id="home" class="navbar-custom bg-color" v-else>
     <div class="container-fluid">
       <div class="row text-white">
-        <div class="col-4 d-flex justify-content-center align-items-center">
+        <div class="col-4 d-flex justify-content-end align-items-center">
           <a class="nav-link m-3" href="#">HOME</a>
           <a class="nav-link m-3" href="#about">ABOUT</a>
           <a class="nav-link m-3" href="#services">SERVICES</a>
           <a class="nav-link m-3" href="#projects">PROJECTS</a>
         </div>
-        <div class="col-3 d-flex justify-content-center align-items-center">
+        <div class="col-4 d-flex justify-content-center align-items-center">
           <a href="#">
-            <svg width="150" height="60" viewBox="0 0 161 86" fill="none" xmlns="http://www.w3.org/2000/svg"
+            <svg width="150" height="100" viewBox="0 0 161 86" fill="none" xmlns="http://www.w3.org/2000/svg"
               xmlns:xlink="http://www.w3.org/1999/xlink">
               <path d="M0 0H160.911V86H0V0Z" fill="url(#pattern0_464_643)" />
               <path d="M0 0H160.911V86H0V0Z" fill="url(#pattern1_464_643)" />
@@ -167,7 +167,7 @@
             </svg>
           </a>
         </div>
-        <div class="col-5 d-flex align-items-center">
+        <div class="col-4 d-flex justify-content-start align-items-center">
           <a href="#" class="nav-link m-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok"
               viewBox="0 0 16 16">
